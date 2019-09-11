f76a061ab8fcc5f400a267f2c2ec8361e3e8bceb
Nitish Kumar <nitish.kumar@sugaldamani.com>
2019-09-11 17:27:06 +0530

    Parking Lot Init

---
+++ parking_lot.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Initial Version of parking_lot.php Created By:
+ *
+ * User: nitishkumar | <mintu.nitish@gmail.com>
+ * Date: 11/09/19
+ * Time: 3:23 PM
+ */
+
+require __DIR__.'/vendor/autoload.php';
+
+use Symfony\Component\Console\Application;
+
+$app = new Application();
+
+
+$app->run();
---
+++ src/ParkingLot.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Initial Version of ParkingLot.php Created By:
+ *
+ * User: nitishkumar | <nitish.kumar@sugaldamani.com>
+ * Date: 11/09/19
+ * Time: 3:33 PM
+ */
+
+class ParkingLot
+{
+    const SLOT_FILLED = 1;
+    const SLOT_FREE = 0;
+
+    private $capacity;
+    private $slots = [];
+
+    public static function instance($capacity)
+    {
+        static $instance = null;
+
+        if ($instance === null) {
+            $instance = new static($capacity);
+        }
+
+        return $instance;
+    }
+
+    private function __construct($capacity) {
+        for ($i = 0; $i < $this->capacity; $i++) {
+            $this->slots[$i] = self::SLOT_FREE;
+        }
+    }
+
+    private function __clone() {}
+
+    private function verifySlotNumber($slot) {
+        return $slot < $this->capacity;
+    }
+
+    public function getCapacity() {
+        return $this->capacity;
+    }
+
+    public function getSlots() {
+        return $this->slots;
+    }
+
+    public function getFreeSlot() {
+        return array_search(self::SLOT_FREE, $this->slots, true);
+    }
+
+    public function setSlotFilled(int $slot) {
+        $this->slots[$slot] = self::SLOT_FILLED;
+    }
+
+    public function setSlotFree(int $slot) {
+        $this->slots[$slot] = self::SLOT_FREE;
+    }
+}
